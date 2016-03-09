027dbc92105e559fe437c861a2f9bb9b3606449f
Imanol Fernandez <imanolf@ludei.com>
2016-03-09 15:57:05 +0100

    Add tests

--- test/ios/AdTest/AdTest/TestServiceChartboost.m
+++ test/ios/AdTest/AdTest/TestServiceChartboost.m
@@ -1,7 +1,3 @@
-//
-//  TestServiceAdMob.h
-//  AdTest
-//
 //  Created by Imanol Fernandez Gorostizag on 26/1/15.
 //  Copyright (c) 2015 Ludei. All rights reserved.
 //
--- test/ios/AdTest/AdTest/TestServiceHeyzap.m
+++ test/ios/AdTest/AdTest/TestServiceHeyzap.m
@@ -1,13 +1,17 @@
-//
-//  TestServiceHeyzap.m
-//  AdTest
-//
 //  Created by Imanol Fernandez Gorostizag on 9/3/16.
 //  Copyright © 2016 Ludei. All rights reserved.
 //
 
-#import "TestServiceHeyzap.h"
+#import "TestService.h"
+#import "LDAdServiceHeyzap.h"
 
-@implementation TestServiceHeyzap
+@implementation TestService
+
++(LDAdService*) create
+{
+    LDAdServiceHeyzap * heyzap = [[LDAdServiceHeyzap alloc] init];
+    [heyzap configurePublisherId:@"719d975e5c491118535b3413a8b20d52"];
+    return heyzap;
+}
 
 @end
