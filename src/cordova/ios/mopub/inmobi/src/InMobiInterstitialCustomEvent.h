44fef7a22a6048dcc76e647b5b01d8267bad9b54
Roger Solsona <rogersolsonaribes@gmail.com>
2016-05-13 11:49:18 +0200

    adding inmobi sdk and inmobi adapter for mopub

---
+++ src/cordova/ios/mopub/inmobi/src/InMobiInterstitialCustomEvent.h
@@ -0,0 +1,14 @@
+//
+//  InMobiInterstitialCustomEvent.h
+//  InMobi
+//
+
+#import <MoPub/MPInterstitialCustomEvent.h>
+
+#import <InMobi/IMInterstitial.h>
+#import <InMobi/IMInterstitialDelegate.h>
+
+
+@interface InMobiInterstitialCustomEvent : MPInterstitialCustomEvent <IMInterstitialDelegate>
+
+@end
