210c9fe7081891f60e2a3e424668d5a9657dfd5a
Roger Solsona <rogersolsonaribes@gmail.com>
2016-05-18 17:35:21 +0200

    adding android chartboost adapter for Admob

---
+++ src/cordova/android/admob/chartboost/src/ChartboostAdapterPlugin.java
@@ -0,0 +1,39 @@
+package com.ludei.ads.cordova;
+
+import com.chartboost.sdk.Chartboost;
+
+import org.apache.cordova.CordovaPlugin;
+
+
+public class ChartboostAdapterPlugin extends CordovaPlugin {
+	
+	@Override
+    public void onStart() {
+        super.onStart();
+        Chartboost.onStart(cordova.getActivity());
+    }
+
+    @Override
+    public void onResume(boolean multitasking) {
+        super.onResume(multitasking);
+        Chartboost.onResume(cordova.getActivity());
+    }
+
+    @Override
+    public void onPause(boolean multitasking) {
+        super.onPause(multitasking);
+        Chartboost.onPause(cordova.getActivity());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Chartboost.onStop(cordova.getActivity());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Chartboost.onDestroy(cordova.getActivity());
+    }
+};
